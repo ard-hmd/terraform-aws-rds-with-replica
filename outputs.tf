b018068ca5d3e3bcde54716782088fcde0492f63
ardhmd <ard.datascientest@gmail.com>
2023-10-02 14:14:47 +0000

    Updated

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,5 @@
+// outputs.tf dans le module RDS
+
 output "db_instance_arn" {
   description = "The ARN of the RDS instances"
   value       = { for idx, instance in aws_db_instance.my_db_instances : idx => instance.arn }
@@ -13,22 +15,17 @@ output "db_instance_identifier" {
   value       = { for idx, instance in aws_db_instance.my_db_instances : idx => instance.identifier }
 }
 
-output "rds_security_group_ids" {
-  description = "The IDs of the Security Groups associated with the RDS instances."
-  value       = { for idx, sg in aws_security_group.rds_sg : idx => sg.id }
-}
-
 output "replica_db_instance_arn" {
-  description = "The Amazon Resource Name (ARN) of the RDS replica instance."
-  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica-myinstance : idx => instance.arn } : {}
+  description = "The ARN of the RDS replica instances"
+  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica_myinstance : idx => instance.arn } : {}
 }
 
 output "replica_db_instance_endpoint" {
-  description = "The connection endpoint for the RDS replica instance."
-  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica-myinstance : idx => instance.endpoint } : {}
+  description = "The connection endpoints of the RDS replica instances"
+  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica_myinstance : idx => instance.endpoint } : {}
 }
 
 output "replica_db_instance_identifier" {
-  description = "The connection identifier for the RDS replica instance."
-  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica-myinstance : idx => instance.identifier } : {}
+  description = "The identifiers of the RDS replica instances"
+  value       = var.create_replica ? { for idx, instance in aws_db_instance.replica_myinstance : idx => instance.identifier } : {}
 }
